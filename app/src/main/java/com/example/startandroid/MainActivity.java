febf0f472087bed755b2e6b5c54051c31fec8be1
Nevimax <nevimax@gmail.com>
2020-03-05 12:09:19 +0200

    start

---
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.startandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//package com.example.startandroid;
+//
+//        import androidx.appcompat.app.AppCompatActivity;
+//
+//        import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
